9764a77abc8dfe365bf9b881e882f627e4d455a5
Guilherme Tiscoski <github@guilhermetiscoski.com>
2026-05-04 09:42:17 -0500

    fix: add style customization example

--- neovim.lua
+++ neovim.lua
@@ -3,6 +3,14 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
+		-- Customize theme specs, e.g: disable italic. For full spec: https://github.com/rose-pine/neovim
+		-- config = function()
+		-- require("rose-pine").setup({
+		--  styles = {
+		--    italic = false,
+		--  },
+		-- })
+		-- end,
 	},
 	{
 		"LazyVim/LazyVim",
