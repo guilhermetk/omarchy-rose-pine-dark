b5685c550956ce269d7a33be42434ebc97b2acc8
Guilherme Tiscoski <github@guilhermetiscoski.com>
2026-05-04 09:34:18 -0500

    feat: move to new colors.toml system

--- neovim.lua
+++ neovim.lua
@@ -2,14 +2,7 @@ return {
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
-		-- Customize theme specs, e.g: disable italic. For full spec: https://github.com/rose-pine/neovim
-		-- config = function()
-		-- require("rose-pine").setup({
-		--  styles = {
-		--    italic = false,
-		--  },
-		-- })
-		-- end,
+		priority = 1000,
 	},
 	{
 		"LazyVim/LazyVim",
